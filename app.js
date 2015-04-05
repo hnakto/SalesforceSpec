14f08d338e2a84b887a3cb46104500f2f6b1ff1a
shaga <shaga@pasonatquila.com>
2015-04-05 23:42:59 +0900

    bug-fix. header setting

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var fs = Promise.promisifyAll(require("fs"));
 var Log = require('log');
 var log = new Log(Log.DEBUG);
 var yaml = require('js-yaml');
+var moment = require('moment');
 
 var SpreadSheet = require('./lib/SpreadSheet');
 var SalesforceSpec = require('./lib/SalesforceSpec');
@@ -53,15 +54,24 @@ Promise.all([
 });
 
 function build_page_layout(){
+    var documentname = config.output_file.page_layout.replace('.xlsx','');
+    set_header(spread_page_layout, documentname, '-');
     spread_page_layout.bulk_copy_sheet('base', spec.object_names);
     _.each(spec.object_names, function(obj_name){
         var obj = spec.page_layouts()[obj_name];
         var row_number = 7;
-        var header_value = ['','','','','','','','','','','','','','','','','','',spec.object_label()[obj_name] + '(' + obj_name + ')'];
-        spread_page_layout.set_row(obj_name,3,header_value);
         _.each(Object.keys(obj), function(field_name){
             var field = obj[field_name];
             var insert_row0 = ['', '項目名', '','','','','','型',''];
+            var layouts = Object.keys(field);
+            layouts = _.map(layouts, function(e){return e.replace(obj_name+'-','')});
+            spread_page_layout.set_row(obj_name,3,insert_row0.concat(layouts));
+            spread_page_layout.set_row(obj_name,3,
+                [config.system_name,'','','','','','','','',documentname,'','','','','','','','',
+                    spec.label_name(obj_name) + '(' + obj_name + ')','','','','','','','','',moment().format("YYYY/MM/DD"),'','','','']);
+
+            spread_page_layout.set_row(obj_name,6,insert_row0.concat(layouts));
+            
             var insert_row1 = ['', spec.field_label()[obj_name+'.'+field_name],'','',field_name, '', '', spec.field_type()[obj_name+'.'+field_name], '参照可能'];
             var insert_row2 = ['', spec.field_label()[obj_name+'.'+field_name],'','',field_name, '', '', spec.field_type()[obj_name+'.'+field_name], '参照のみ'];
             _.each(Object.keys(field), function(layout_name){
@@ -78,7 +88,7 @@ function build_page_layout(){
                     insert_row2.push('');
                 }
             });
-            spread_page_layout.set_row(obj_name,6,insert_row0);
+            
             spread_page_layout.set_row(obj_name,row_number++,insert_row1);
             spread_page_layout.set_row(obj_name,row_number++,insert_row2);
         });
@@ -89,7 +99,9 @@ function build_page_layout(){
 
 function build_object_permission(){
     var profiles = spec.valid_profile();
+    set_header(spread_crud, config.output_file.object_permission.replace('.xlsx',''), '-');
     spread_crud.set_row('base',6,['','オブジェクト','','','','','','CRUD'].concat(profiles));
+
     var index_on_mark = spread_crud.shared_strings.add_string('●');
     var mark = {'●':index_on_mark};
     var object_permission = spec.object_permission();
@@ -125,6 +137,11 @@ function build_object_permission(){
 
 function build_workflow(){
     var row_number = 10;
+    set_header(spread_workflow, config.output_file.workflow.replace('.xlsx',''), '-');
+    spread_workflow.set_row('base',6,['','','評価条件']);
+    spread_workflow.set_row('base',7,['','','','1 : 作成されたとき   2 : 作成されたとき、および編集されるたび   3 : 作成されたとき、およびその後基準を満たすように編集されたとき']);
+    spread_workflow.set_row('base',9,['','No','有効','オブジェクト','','','ワークフロー名','','','','評価条件','条件','','','説明','','','','','','','アクション種別','','アクション名','','','表示名 / 説明','','','','','対象項目 / 受信対象','','更新値 / 送信テンプレート']);
+    
     _.each(Object.keys(spec.workflow()), function(objectname){
         var workflow_in_object = spec.workflow()[objectname];
         _.each(workflow_in_object, function(action){
@@ -143,6 +160,12 @@ function build_workflow(){
 
 function build_validation(){
     var row_number = 7;
+    set_header(spread_validation_rule, config.output_file.validation.replace('.xlsx',''), '-');
+    spread_validation_rule.set_row(
+        'base',6,
+        ['','No.','有効','オブジェクト','','','入力規則名','','','','エラー表示場所','','',
+            '評価条件 / 数式','','','','','','','エラーメッセージ','','','','','','','説明']
+    );
     var validation_rules = spec.validation_rule();
     _.each(Object.keys(validation_rules), function(objectname){
         var rules_in_object = validation_rules[objectname];
@@ -159,18 +182,21 @@ function build_validation(){
 }
 
 function build_custom_field(){
+    var documentname = config.output_file.custom_field.replace('.xlsx','');
+    set_header(spread_custom_field, documentname, '-');
+    spread_custom_field.set_row('base',6,
+        ['','No','表示ラベル','','','','API参照名','','','','型','','数式/選択リスト値','','','',
+         '初期値','','','必須','ユニーク','外部','履歴','トレンド','説明']);
     spread_custom_field.bulk_copy_sheet('base', spec.object_names);
     var all_custom_fields = spec.custom_field();
     _.each(spec.object_names, function(obj_name){
         var row_number = 7;
-        spread_custom_field.set_row(
-            obj_name,
-            3,
-            ['','','','','','','','','','','','','','','','','','',spec.object_label(obj_name) + '(' + obj_name + ')']
-        );
+        spread_custom_field.set_row(obj_name,3,
+            [config.system_name,'','','','','','','','',documentname,'','','','','','','','',
+                spec.label_name(obj_name)+'('+obj_name+')','','','','','','','','',moment().format("YYYY/MM/DD"),'','','','']);
+
         var custom_fields = all_custom_fields[obj_name];
         _.each(custom_fields, function(field){
-            console.log(field.label);
             spread_custom_field.set_row(
                 obj_name,
                 row_number++,
@@ -183,9 +209,16 @@ function build_custom_field(){
 }
 
 function build_field_permission(){
+    var documentname = config.output_file.field_permission.replace('.xlsx','');
+    set_header(spread_field_permission, documentname, '-');
+
     spread_field_permission.bulk_copy_sheet('base', spec.object_names);
     var index_on_mark = spread_field_permission.shared_strings.add_string('●');
     _.each(spec.object_names, function(sheetname){
+        spread_field_permission.set_row(sheetname,3,
+            [config.system_name,'','','','','','','','',documentname,'','','','','','','','',
+                spec.label_name(sheetname)+'('+sheetname+')','','','','','','','','',moment().format("YYYY/MM/DD"),'','','','']);
+
         var profiles = spec.valid_profile();
         var field_permissions = spec.field_permission();
         var fields = spec.custom_field()[sheetname];
@@ -223,6 +256,21 @@ function build_field_permission(){
         });
     });
     return fs.writeFileAsync(config.output_directory + config.output_file.field_permission,spread_field_permission.generate());
+}
+
+function set_header(spreadsheet, documentname, document_target){
+    spreadsheet.set_row('base',1,
+        ['システム名','','','','','','','','','ドキュメント名','','','','','','','','',    
+         'ドキュメント対象','','','','','','','','','作成日','','','','最終更新日']);    
+    spreadsheet.set_row('base',2,    
+        ['システム名','','','','','','','','','ドキュメント名','','','','','','','','',    
+         'ドキュメント対象','','','','','','','','','作成者','','','','最終更新者']);
+    spreadsheet.set_row('base',3,
+        [config.system_name,'','','','','','','','',documentname,'','','','','','','','',
+         document_target,'','','','','','','','',moment().format("YYYY/MM/DD"),'','','','']);
+    spreadsheet.set_row('base',4,
+        [config.system_name,'','','','','','','','',documentname,'','','','','','','','',
+         document_target,'','','','','','','','',config.created_by,'','','','']);
 }
 
 function load_config(){
